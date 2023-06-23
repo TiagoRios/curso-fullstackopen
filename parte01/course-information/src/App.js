ed70375ab24355f00d3bd3bae4d432e931aa703d
Tiago Rios <tiago_pc9@hotmail.com>
2023-06-23 15:53:02 -0300

    Parte01: exercício 1.1 e 1.2 concluídos.

--- parte01/course-information/src/App.js
+++ parte01/course-information/src/App.js
@@ -1,15 +1,41 @@
 import React from 'react';
 
 export default function App() {
-    let corTexto = { color: "red" };
+
+    const course = 'Desenvolvimento de aplicação Half Stack'
+    const part1 = 'Fundamentos da biblioteca React'
+    const exercises1 = 10
+    const part2 = 'Usando props para passar dados'
+    const exercises2 = 7
+    const part3 = 'Estado de um componente'
+    const exercises3 = 14
 
     return (
-        <>
-            <h1>Molde para os novos projetos</h1>
-            <ul>
-                <li style={corTexto}>Mude o nome do projeto nos arquivos package*.json</li>
-                <li style={corTexto}>Mude o nome do projeto em public/index.html</li>
-            </ul>
-        </>
+        <div>
+            <Header course={course}></Header>
+
+            <Content part={part1} exercises={exercises1}></Content>
+            <Content part={part2} exercises={exercises2}></Content>
+            <Content part={part3} exercises={exercises3}></Content>
+
+            <Total>{exercises1 + exercises2 + exercises3}</Total>
+        </div>
     )
+}
+
+function Header({ course }) {
+    return (<h1>{course}</h1>);
+}
+
+function Content(props) {
+    // return (<p>{part} - {exercises}</p>) // simples
+    return (<Part {...props}></Part>);
+}
+
+function Total({children}) {
+    return (<p>Number of exercises {children}</p>)
+}
+
+function Part({part, exercises}){
+    return (<p>{part} - {exercises}</p>);
 }
