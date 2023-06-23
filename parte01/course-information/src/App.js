fa96e86a0cf522fdb3074b29ca5087969edfba41
Tiago Rios <tiago_pc9@hotmail.com>
2023-06-23 17:10:29 -0300

    Parte01: exercícios de 1.3 a 1.5 concluídos.

--- parte01/course-information/src/App.js
+++ parte01/course-information/src/App.js
@@ -2,40 +2,67 @@ import React from 'react';
 
 export default function App() {
 
-    const course = 'Desenvolvimento de aplicação Half Stack'
-    const part1 = 'Fundamentos da biblioteca React'
-    const exercises1 = 10
-    const part2 = 'Usando props para passar dados'
-    const exercises2 = 7
-    const part3 = 'Estado de um componente'
-    const exercises3 = 14
+    const course = {
+        name: 'Desenvolvimento de aplicação Half Stack',
+        parts: [
+            {
+                name: 'Fundamentos da biblioteca React',
+                exercises: 10
+            },
+            {
+                name: 'Usando props para passar dados',
+                exercises: 7
+            },
+            {
+                name: 'Estado de um componente',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <div>
-            <Header course={course}></Header>
+            <Header course={course.name} />
 
-            <Content part={part1} exercises={exercises1}></Content>
-            <Content part={part2} exercises={exercises2}></Content>
-            <Content part={part3} exercises={exercises3}></Content>
+            <ContentList parts={course.parts} />
 
-            <Total>{exercises1 + exercises2 + exercises3}</Total>
+            <Total parts={course.parts} />
         </div>
     )
 }
+
+/* ========================= Header =========================== */
 
 function Header({ course }) {
     return (<h1>{course}</h1>);
 }
 
+/* ========================= Content ========================== */
+
+function ContentList({ parts }) {
+    const arrList = parts.map((part, index) => {
+        return <li key={index}><Content part={part} /></li>
+    })
+
+    return (
+        <ul>{arrList}</ul>
+    )
+}
+
 function Content(props) {
-    // return (<p>{part} - {exercises}</p>) // simples
     return (<Part {...props}></Part>);
 }
 
-function Total({children}) {
-    return (<p>Number of exercises {children}</p>)
+function Part({ part }) {
+    return (<>{part.name} - {part.exercises}</>);
 }
 
-function Part({part, exercises}){
-    return (<p>{part} - {exercises}</p>);
+/* ========================= Total ============================ */
+
+function Total({ parts }) {
+    const arrTotal = parts.reduce((anterior, atual) => {
+        return anterior += atual.exercises
+    }, 0)
+
+    return (<p>Number of exercises {arrTotal}</p>)
 }
