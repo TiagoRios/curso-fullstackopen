45a96a3864296db52099dcfcc14895962e0e759d
Tiago Rios <tiago_pc9@hotmail.com>
2023-10-10 14:21:38 -0300

    Parte04: projeto bloglist - exercício 4.3 - concluído.

---
+++ parte04/bloglist/tests/list_helper.spec.js
@@ -0,0 +1,8 @@
+import listHelper from '../utils/list_helper.js'
+
+test('dummy returns one', () => {
+    const blogs = []
+    const result = listHelper.dummy(blogs);
+
+    expect(result).toBe(1)
+})
---
+++ parte04/bloglist/utils/list_helper.js
@@ -0,0 +1,7 @@
+const dummy = (blogs) => {
+    if (blogs.length === 0) return 1;
+
+    return 1;
+}
+
+export default { dummy };
