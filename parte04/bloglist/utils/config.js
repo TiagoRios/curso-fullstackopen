bc558d58a0a31665c15c9f2517aa1499a3fd8a1e
Tiago Rios <tiago_pc9@hotmail.com>
2023-10-13 15:27:36 -0300

    Parte04: projeto bloglist - configuração DB separado para testes.

--- parte04/bloglist/utils/config.js
+++ parte04/bloglist/utils/config.js
@@ -2,7 +2,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { PORT, MONGODB_URI } = process.env;
+const { PORT } = process.env;
+
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI
 
 export default {
     PORT, MONGODB_URI
